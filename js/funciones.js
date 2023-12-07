dbaefd63c6e8a7c13fd2d5a1123b90dc9c6e83da
flaviofonck <flaviofonck@gmail.com>
2023-12-07 09:57:39 -0500

    agenda curso gapps v9

--- js/funciones.js
+++ js/funciones.js
@@ -6,3 +6,9 @@ function doGet()
 function obtenerDatosHTML(archivo){
     return HtmlService.createHtmlOutputFromFile(archivo).getContent();
 }
+
+function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('1DOqIIGMbvXANkNTjmvhB9t366-oJrEFEeZqM4fTjIEE').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
