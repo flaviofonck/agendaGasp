65f97d3b2be8c5dbfaef412a60ddf1f6ff85448c
flaviofonck <flaviofonck@gmail.com>
2023-12-07 17:03:15 -0500

    agenda curso gapps v9 3

--- js/funciones.js
+++ js/funciones.js
@@ -1,14 +1,27 @@
+const HOJA = SpreadsheetApp.openById('1DOqIIGMbvXANkNTjmvhB9t366-oJrEFEeZqM4fTjIEE').getActiveSheet();
+
 function doGet()
     {
         return HtmlService.createTemplateFromFile('html/web').evaluate().setTitle('Agenda Google App Script');
     }
 
-function obtenerDatosHTML(archivo){
+function doPost(datos)
+{
+    insertarContacto(datos.parameter.nombre, datos.parameter.correo, datos.parameter.telefono);
+    return HtmlService.createTemplateFromFile('html/web').evaluate().setTitle('Agenda Google App Script');
+}
+
+
+    function obtenerDatosHTML(archivo){
     return HtmlService.createHtmlOutputFromFile(archivo).getContent();
 }
 
-function obtenerContactos(){
-    let hoja = SpreadsheetApp.openById('1DOqIIGMbvXANkNTjmvhB9t366-oJrEFEeZqM4fTjIEE').getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    return datos;
+function obtenerContactos()
+{
+    return HOJA.getDataRange().getValues();
+}
+
+function insertarContacto(nombre, correo, telefono)
+{
+    HOJA.appendRow([nombre, correo, telefono]);
 }
