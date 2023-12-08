f427929c79c1ee2a254fe2265b895e66e11a4912
flaviofonck <flaviofonck@gmail.com>
2023-12-08 08:44:37 -0500

    version modificar contacto

--- js/funciones.js
+++ js/funciones.js
@@ -21,7 +21,21 @@ function obtenerContactos()
     return HOJA.getDataRange().getValues();
 }
 
-function insertarContacto(nombre, correo, telefono)
+function insertarContacto(nombre,apellido, correo, telefono)
 {
-    HOJA.appendRow([nombre, correo, telefono]);
+    HOJA.appendRow([nombre,apellido, correo, telefono]);
 }
+
+function borrarContacto(numfila){
+    HOJA.deleteRow(numfila);
+}
+
+function modificarContacto(numfila,datosContacto){
+    let celdas = HOJA.getRange('A'+numfila+':D'+numfila);
+    celdas.setValues([[datosContacto.nombre,datosContacto.apellido,datosContacto.correo,datosContacto.telefono]]);
+    /*HOJA.getRange(numfila,0).setValue(datosContacto.nombre);
+    HOJA.getRange(numfila,1).setValue(datosContacto.apellido);
+    HOJA.getRange(numfila,2).setValue(datosContacto.correo);
+    HOJA.getRange(numfila,3).setValue(datosContacto.telefono);*/
+}
+
